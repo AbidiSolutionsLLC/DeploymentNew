03de24ad04e2ecd3c84cdcf617bf4e3f2a7f0281
AbidiSolutionsLLC <appadmin@abidisolutions.com>
2026-02-01 18:24:10 -0500

    added domain index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000', "http://localhost:5174","http://localhost:5175" , "http://192.168.100.91:5173"],
+  origin: ['https://abidipro.abidisolutions.com', 'http://localhost:5173', 'http://localhost:3000', "http://localhost:5174","http://localhost:5175" , "http://192.168.100.91:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
