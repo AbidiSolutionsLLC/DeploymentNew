67e9f34364f1db96b80af2f7cb50a946a037f745
AbidiSolutionsLLC <appadmin@abidisolutions.com>
2026-02-06 05:47:41 -0500

    Update dateUtils.js

--- backend/utils/dateUtils.js
+++ backend/utils/dateUtils.js
@@ -1,23 +1,30 @@
 const moment = require('moment-timezone');
 
+/**
+ * Strictly define the company timezone to Eastern Standard Time.
+ * All attendance and timesheet logic will anchor to this.
+ */
 const TIMEZONE = "America/New_York";
 
 /**
  * Returns a Date object representing the start of the day (00:00:00) in EST.
+ * Prevents local/UTC conversion shifts.
  */
 exports.getStartOfESTDay = (date = new Date()) => {
-    return moment(date).tz(TIMEZONE).startOf('day').toDate();
+    return moment.tz(date, TIMEZONE).startOf('day').toDate();
 };
 
 /**
  * Returns a Date object representing the end of the day (23:59:59.999) in EST.
+ * Useful for building 24-hour database query windows.
  */
 exports.getEndOfESTDay = (date = new Date()) => {
-    return moment(date).tz(TIMEZONE).endOf('day').toDate();
+    return moment.tz(date, TIMEZONE).endOf('day').toDate();
 };
 
 /**
  * Returns the current time in EST as a Moment object.
+ * Used for check-in/out logic.
  */
 exports.getCurrentESTTime = () => {
     return moment().tz(TIMEZONE);
@@ -25,11 +32,16 @@ exports.getCurrentESTTime = () => {
 
 /**
  * Checks if a given date is a weekend in the EST timezone.
+ * Used to restrict check-ins on Saturday and Sunday.
  */
 exports.isESTWeekend = (date = new Date()) => {
-    const day = moment(date).tz(TIMEZONE).day();
+    const day = moment.tz(date, TIMEZONE).day();
     return day === 0 || day === 6; // 0 = Sunday, 6 = Saturday
 };
 
+/**
+ * Export raw moment and TIMEZONE string for use in controller validation logic.
+ * Needed for exports like moment(date).tz(TIMEZONE).format('YYYY-MM-DD').
+ */
 exports.moment = moment;
 exports.TIMEZONE = TIMEZONE;
