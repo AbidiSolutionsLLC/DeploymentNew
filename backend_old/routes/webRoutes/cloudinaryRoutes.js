6570ca449bd2c7b0a7db22789cda8664a93b7832
mtayyabsaleem <tayyabsknowledge985@gmail.com>
2026-03-05 12:48:41 -0500

    checkin/checkout

--- backend_old/routes/webRoutes/cloudinaryRoutes.js
+++
@@ -1,5 +0,0 @@
-// // routes/cloudinary.routes.js
-// const router = require('express').Router();
-// const ctrl   = require('../../controllers/cloudinaryController');
-// router.post('/signUpload', ctrl.signUpload);
-// module.exports = router;
